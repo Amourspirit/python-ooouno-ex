7df63cf096e58336beed33972334f1941838d029
Paul Moss <vibrationoflife@protonmail.com>
2022-03-26 14:14:15 -0400

    update .gitignore to include src/lib

---
+++ src/lib/enums.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+from enum import IntEnum
+
+
+class CompareEnum(IntEnum):
+    """Compare Enumeration"""
+
+    AFTER = 1
+    BEFORE = -1
+    EQUAL = 0
+
+
+class CompareEnum(IntEnum):
+    """Compare Enumeration"""
+
+    AFTER = 1
+    BEFORE = -1
+    EQUAL = 0
