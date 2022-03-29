07d9c3021dabaea1061d83bceb9ade619b806dfc
Paul Moss <vibrationoflife@protonmail.com>
2022-03-28 20:10:24 -0400

    remove field from model args

--- src/models/example/args.py
+++ src/models/example/args.py
@@ -1,13 +1,11 @@
 # coding: utf-8
 from pydantic import BaseModel, Field, validator
-from typing import Optional, List
+from typing import List
 from .. import validators
 
 class ExampleArgs(BaseModel):
     src_file: str
     output_name: str
-    embed_in_doc: bool
-    document_name: Optional[str] = None
     include_modules: List[str] = Field(default_factory=list)
     include_paths: List[str] = Field(default_factory=list)
     remove_modules: List[str] = Field(default_factory=list)
