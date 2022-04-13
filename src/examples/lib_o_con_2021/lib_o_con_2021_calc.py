35d71e95ad7e2565bcf6435ddba3d1ff8190a97e
Barry-Thomas-Paul: Moss <4193389+Amourspirit@users.noreply.github.com>
2022-04-12 22:11:58 -0400

    Calc path (#24)
    Added automatic path to res dir.

--- src/examples/lib_o_con_2021/lib_o_con_2021_calc.py
+++ src/examples/lib_o_con_2021/lib_o_con_2021_calc.py
@@ -1,4 +1,4 @@
-"""
+'''
 Module to demonstrate SourceForge Methods in the Calc Service	
 	
 Activate	in create_sheet_example
@@ -30,17 +30,16 @@ SetArray	in create_random_matrix_v2
 SetValue	in create_random_matrix_v1
 SetCellStyle	in mark_invalid
 SetFormula	see SetValue
-SortRange	"""
+SortRange	'''
 from ooo.lo.sheet.x_spreadsheet import XSpreadsheet
+from ooo.lo.sheet.x_sheet_cell_cursor import XSheetCellCursor
+from ooo.lo.sheet.sheet_cell_range import SheetCellRange
+from ooo.lo.frame.x_model import XModel
 
-
-
-
-
-
+from typing import Union
 import scriptforge as SF
 import random as rnd
-
+from pathlib import Path
 
 # Creates a 6x6 matrix starting at A1
 def create_random_matrix_v1(args=None):
@@ -54,15 +53,13 @@ def create_random_matrix_v1(args=None):
             else:
                 doc.SetValue(target_cell, "ODD")
 
-
 # Creates a 6x6 matrix starting at A1
 # Uses the method setArray to insert values
 def create_random_matrix_v2(args=None):
     doc: SF.SFDocuments.SF_Calc = SF.CreateScriptService("Calc")
-    rnd_word = lambda: "EVEN" if rnd.random() < 0.5 else "ODD"
+    rnd_word = lambda : "EVEN" if rnd.random() < 0.5 else "ODD"
     values = [[rnd_word() for _ in range(6)] for _ in range(6)]
     doc.SetArray("A1", values)
-
 
 # Creates an mxn matrix starting at A1 and asks the desired size
 def create_random_matrix_v3(args=None):
@@ -79,7 +76,6 @@ def create_random_matrix_v3(args=None):
             else:
                 doc.SetValue(target_cell, "ODD")
 
-
 # Creates an mxn matrix starting at A1 and asks the desired size
 # Uses the method setArray to insert values
 def create_random_matrix_v4(args=None):
@@ -87,20 +83,19 @@ def create_random_matrix_v4(args=None):
     bas: SF.SFScriptForge.SF_Basic = SF.CreateScriptService("Basic")
     n_rows = bas.InputBox("Number of rows")
     n_cols = bas.InputBox("Number of columns")
-    rnd_word = lambda: "EVEN" if rnd.random() < 0.5 else "ODD"
-    values = [[rnd_word() for _ in range(int(n_cols))] for _ in range(int(n_rows))]
-    doc.SetArray("A1", values)
-
+    rnd_word = lambda : "EVEN" if rnd.random() < 0.5 else "ODD"
+    values = [[rnd_word() for _ in range(int(n_cols))]
+              for _ in range(int(n_rows))]
+    doc.setArray("A1", values)
 
 # Clear region starting at A1
 def clear_region_a1(args=None):
     doc: SF.SFDocuments.SF_Calc = SF.CreateScriptService("Calc")
-    cur_sheet: XSpreadsheet = XSCRIPTCONTEXT.getDocument().CurrentController.ActiveSheet
+    cur_sheet: XSpreadsheet  = XSCRIPTCONTEXT.getDocument().CurrentController.ActiveSheet
     cell = cur_sheet.getCellRangeByName("A1")
-    cursor = cur_sheet.createCursorByRange(cell)
+    cursor: Union[SheetCellRange, XSheetCellCursor] = cur_sheet.createCursorByRange(cell)
     cursor.collapseToCurrentRegion()
     doc.ClearAll(cursor.AbsoluteName)
-
 
 # Creates a matrix of size 10x8 with random integers between -20 and 100
 def create_values_for_example_2(args=None):
@@ -109,14 +104,13 @@ def create_values_for_example_2(args=None):
     data = [[rnd.randint(-20, 100) for _ in range(8)] for _ in range(10)]
     doc.SetArray("A1", data)
 
-
 # Marks cells with negative values as INVALID and apply the 'Bad' cell style
 def mark_invalid(args=None):
     doc: SF.SFDocuments.SF_Calc = SF.CreateScriptService("Calc")
     # Gets address of current selection
     cur_selection: str = doc.CurrentSelection
     # Gets address of first cell in the selection
-    first_cell = doc.Offset(cur_selection, 0, 0, 1, 1)
+    _ = doc.Offset(cur_selection, 0, 0, 1, 1)
     for i in range(doc.Height(cur_selection)):
         for j in range(doc.Width(cur_selection)):
             cell = doc.Offset(cur_selection, i, j, 1, 1)
@@ -125,36 +119,30 @@ def mark_invalid(args=None):
                 doc.SetValue(cell, "INVALID")
                 doc.SetCellStyle(cell, "Bad")
 
-
 # Example of using ClearAll
 def clear_contents_v1(args=None):
     doc: SF.SFDocuments.SF_Calc = SF.CreateScriptService("Calc")
-    doc.clearAll("B2:B7")
-
+    doc.ClearAll("B2:B7")
 
 # Example of using ClearFormats
 def clear_contents_v2(args=None):
     doc: SF.SFDocuments.SF_Calc = SF.CreateScriptService("Calc")
-    doc.clearFormats("D2:D7")
-
+    doc.ClearFormats("D2:D7")
 
 # Example of using ClearValues
 def clear_contents_v3(args=None):
     doc: SF.SFDocuments.SF_Calc = SF.CreateScriptService("Calc")
-    doc.clearValues("F2:F7")
-
+    doc.ClearValues("F2:F7")
 
 # Copying to a single cell
 def copy_cells_v1(args=None):
     doc: SF.SFDocuments.SF_Calc = SF.CreateScriptService("Calc")
-    doc.copyToCell("A1:A4", "C1")
-
+    doc.CopyToCell("A1:A4", "C1")
 
 # Copying cells into a larger range
 def copy_cells_v2(args=None):
     doc: SF.SFDocuments.SF_Calc = SF.CreateScriptService("Calc")
-    doc.copyToRange("A1:A4", "E1:F6")
-
+    doc.CopyToRange("A1:A4", "E1:F6")
 
 # Copies range from an open file
 def copy_range_from_file(args=None):
@@ -167,33 +155,28 @@ def copy_range_from_file(args=None):
     # Pastes the contents into the destination
     doc.CopyToCell(source_range, "A1")
 
-
 # Inserting new sheet
 def create_sheet_example(args=None):
     doc: SF.SFDocuments.SF_Calc = SF.CreateScriptService("Calc")
     doc.InsertSheet("TestSheet", 2)
     doc.Activate("TestSheet")
 
-
 # Copying an existing sheet
 def copy_sheet_example(args=None):
     doc: SF.SFDocuments.SF_Calc = SF.CreateScriptService("Calc")
     doc.CopySheet("TestSheet", "Copy_TestSheet")
-
 
 # Removing a sheet
 def remove_sheet_example(args=None):
     doc: SF.SFDocuments.SF_Calc = SF.CreateScriptService("Calc")
     doc.RemoveSheet("Copy_TestSheet")
 
-
 # Copies sheet from another file (open or closed)
 def copy_from_file_example(args=None):
     doc: SF.SFDocuments.SF_Calc = SF.CreateScriptService("Calc")
-    doc.CopySheetFromFile(
-        "/home/rafael/Documents/DataSource.ods", "Sheet2", "Copy_Sheet2"
-    )
-
+    res_path = get_res_path(doc)
+    wb = str(Path(res_path, "DataSource.ods"))
+    doc.CopySheetFromFile(wb, "Sheet2", "Copy_Sheet2")
 
 # Example using the DAvg method
 def calculate_average(args=None):
@@ -202,21 +185,40 @@ def calculate_average(args=None):
     result = doc.DAvg("A1:E1")
     bas.MsgBox("The average is {:.02f}".format(result))
 
-
 # Open CSV file JobData_v1.csv using default configuration
 def open_csv_file_v1(args=None):
     doc: SF.SFDocuments.SF_Calc = SF.CreateScriptService("Calc")
-    doc.ImportFromCSVFile("/home/rafael/Documents/JobData_v1.csv", "A1")
-
+    res_path = get_res_path(doc)
+    csvfile = str(Path(res_path, "JobData_v1.csv"))
+    doc.ImportFromCSVFile(csvfile, "A1")
 
 # Open CSV file JobData_v2.csv using default configuration
 def open_csv_file_v2(args=None):
     doc: SF.SFDocuments.SF_Calc = SF.CreateScriptService("Calc")
-    doc.ImportFromCSVFile("/home/rafael/Documents/JobData_v2.csv", "A1")
-
+    res_path = get_res_path(doc)
+    csvfile = str(Path(res_path, "JobData_v2.csv"))
+    doc.ImportFromCSVFile(csvfile, "A1")
 
 # Open CSV file using custom configuration
 def open_csv_file_v3(args=None):
     doc: SF.SFDocuments.SF_Calc = SF.CreateScriptService("Calc")
+    res_path = get_res_path(doc)
+    csvfile = str(Path(res_path, "JobData_v2.csv"))
     filter_option = "59,34,UTF-8,1"
-    doc.ImportFromCSVFile("/home/rafael/Documents/JobData_v2.csv", "A1", filter_option)
+    doc.ImportFromCSVFile(csvfile, "A1", filter_option)
+
+# gets the path to res dir
+def get_res_path(doc: SF.SFDocuments.SF_Calc) -> Path:
+    try:
+        cmp: XModel = doc.XComponent
+        url = cmp.getURL()
+        print("url", url)
+        bas: SF.SFScriptForge.SF_Basic = SF.CreateScriptService("Basic")
+        file = bas.ConvertFromUrl(url)
+        print("file", file)
+        doc_path = Path(file)
+        doc_dir = doc_path.parent
+    except Exception as e:
+        print(e)
+        raise
+    return doc_dir / 'res'
