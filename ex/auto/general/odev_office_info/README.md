a642995513de1c37df6bd16d7620a0daeeab8ca0
Barry-Thomas-Paul: Moss <4193389+Amourspirit@users.noreply.github.com>
2022-07-14 14:39:52 -0400

    Develop (#58)

---
+++ ex/auto/general/odev_office_info/README.md
@@ -0,0 +1,25 @@
+# Office Info
+
+This is a basic example that shows how to get info from Office via the command line.
+
+This demo uses [OOO Development Tools](https://python-ooo-dev-tools.readthedocs.io/en/latest/) (ODEV).
+
+ODEV makes this demo possible with just a few lines of code.
+
+See Also: [Examining Office](https://python-ooo-dev-tools.readthedocs.io/en/latest/odev/part1/chapter03.html)
+
+See [source code](./start.py)
+
+## Automate
+
+```sh
+python -m main auto --process "ex/auto/general/odev_office_info/start.py"
+```
+
+or for other commands
+
+```sh
+python -m main auto --process "ex/auto/general/odev_office_info/start.py -h"
+```
+
+![command line example](https://user-images.githubusercontent.com/4193389/179056343-deafd3b5-c16e-45fa-9e2d-c95a0dc6b71e.gif)
---
+++ ex/auto/general/odev_office_info/start.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+# coding: utf-8
+# region Imports
+from __future__ import annotations
+import argparse
+import platform
+
+# debugging
+# from pathlib import Path
+# sys.path.insert(0, Path(__file__).parent.parent.parent.parent.parent)
+
+from ooodev.utils.lo import Lo
+from ooodev.utils.info import Info
+
+# endregion Imports
+
+# region args
+def args_add(parser: argparse.ArgumentParser) -> None:
+    parser.add_argument(
+        "-s",
+        "--services",
+        help="Show Services",
+        action="store_true",
+        dest="services",
+        default=False,
+    )
+    parser.add_argument(
+        "-w",
+        "--write-services",
+        help="Show Write Services",
+        action="store_true",
+        dest="services_write",
+        default=False,
+    )
+    parser.add_argument(
+        "-c",
+        "--calc-services",
+        help="Show Calc Services",
+        action="store_true",
+        dest="services_calc",
+        default=False,
+    )
+    parser.add_argument(
+        "-f",
+        "--filters",
+        help="Show Filters",
+        action="store_true",
+        dest="filters",
+        default=False,
+    )
+
+
+# endregion args
+
+# region Show
+
+
+def show_filters() -> None:
+    print()
+    print(" File Filter Names for Office: ".center(50, "-"))
+    filters = Info.get_filter_names()
+    for filter in filters:
+        print(f"  {filter}")
+    print("-----------")
+    print(f"No. of filters: {len(filter)}")
+
+
+def show_services(title: str, srv_name: str | None = None) -> None:
+    print()
+    print(title.center(50, "-"))
+    services = Info.get_service_names(srv_name)
+    for service in services:
+        print(f"  {service}")
+    print("-----------")
+    print(f"No. of services: {len(services)}")
+
+
+# endregion Show
+
+# region Main
+def main() -> int:
+    # create parser to read terminal input
+    parser = argparse.ArgumentParser(description="main")
+
+    # add args to parser
+    args_add(parser=parser)
+
+    # read the current command line args
+    args = parser.parse_args()
+
+    with Lo.Loader(Lo.ConnectSocket(headless=True)) as loader:
+        print(f"OS Platform: {platform.platform()}")
+        print(f"OS Version: {platform.version()}")
+        print(f"OS Release: {platform.release()}")
+        print(f"OS Architecture: {platform.architecture()}")
+
+        print(f"\nOffice Name: {Info.get_config('ooName')}")
+        print(f"\nOffice version (long): {Info.get_config('ooSetupVersionAboutBox')}")
+        print(f"Office version (short): {Info.get_config('ooSetupVersion')}")
+        print(f"\nOffice language location: {Info.get_config('ooLocale')}")
+        print(f"System language location: {Info.get_config('ooSetupSystemLocale')}")
+
+        print(f"\nWorking Dir: {Info.get_paths('Work')}")
+        print(f"\nOffice Dir: {Info.get_office_dir()}")
+        print(f"\nAddin Dir: {Info.get_paths('Addin')}")
+        print(f"\nFilters Dir: {Info.get_paths('Filter')}")
+        print(f"\nTemplates Dirs: {Info.get_paths('Template')}")
+        print(f"\nGallery Dir: {Info.get_paths('Gallery')}")
+        # see: https://wiki.openoffice.org/w/index.php?title=Documentation/DevGuide/OfficeDev/Path_Settings
+
+        if args.filters:
+            show_filters()
+        if args.services:
+            show_services(" Services for Office: ")
+            # over 900 printed
+        if args.services_write:
+            show_services(" Services for Write: ", Lo.Service.WRITER)
+        if args.services_calc:
+            show_services(" Services for Calc: ", Lo.Service.CALC)
+
+        print()
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
+
+# endregion main
