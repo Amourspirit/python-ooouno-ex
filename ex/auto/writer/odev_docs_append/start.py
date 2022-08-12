f47084f58ef4291f85da78f2a0efadf93de677f3
Barry-Thomas-Paul: Moss <vibrationoflife@protonmail.com>
2022-08-12 18:02:56 -0400

    docs append example

---
+++ ex/auto/writer/odev_docs_append/start.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Print all the text in every paragraph using enumeration access.
+
+from __future__ import annotations
+import sys
+import argparse
+from typing import cast, Any
+
+
+from ooodev.office.write import Write
+from ooodev.utils.file_io import FileIO
+from ooodev.utils.info import Info
+from ooodev.utils.lo import Lo
+from ooodev.wrapper.break_context import BreakContext
+
+from com.sun.star.document import XDocumentInsertable
+from com.sun.star.text import XTextDocument
+
+
+def args_add(parser: argparse.ArgumentParser) -> None:
+    parser.add_argument(
+        "-f",
+        "--file",
+        help="File path of input file to convert",
+        action="store",
+        dest="file_path",
+        required=True,
+    )
+    parser.add_argument(
+        "docs",
+        help="Append one or more documents",
+        nargs='*'
+    )
+
+
+def append_text_files(doc: XTextDocument, *args: str) -> None:
+    cursor = Write.get_cursor(doc)
+    for arg in args:
+        try:
+            cursor.gotoEnd(False)
+            print(f"Appending {arg}")
+            inserter = Lo.qi(XDocumentInsertable, cursor)
+            if inserter is None:
+                print("Document inserter could not be created")
+            else:
+                inserter.insertDocumentFromURL(FileIO.fnm_to_url(arg), ())
+        except Exception as e:
+            print(f"Could not append {arg} : {e}")
+
+
+def main() -> int:
+    # create parser to read terminal input
+    parser = argparse.ArgumentParser(description="main")
+
+    # add args to parser
+    args_add(parser=parser)
+
+    if len(sys.argv) <= 3:
+        parser.print_help()
+        return 0
+
+    # read the current command line args
+    args = parser.parse_args()
+    # print(args)
+    # return
+
+    # Using Lo.Loader context manager wraped by BreakContext load Office and connect via socket.
+    # Context manager takes care of terminating instance when job is done.
+    # see: https://python-ooo-dev-tools.readthedocs.io/en/latest/src/wrapper/break_context.html
+    # see: https://python-ooo-dev-tools.readthedocs.io/en/latest/src/utils/lo.html#ooodev.utils.lo.Lo.Loader
+    with BreakContext(Lo.Loader(Lo.ConnectSocket(headless=True))) as loader:
+
+        fnm = cast(str, args.file_path)
+
+        try:
+            doc = Write.open_doc(fnm=fnm, loader=loader)
+        except Exception as e:
+            print(f"Could not open '{fnm}'")
+            print(f"  {e}")
+            # office will close and with statement is exited
+            raise BreakContext.Break
+
+        try:
+            append_text_files(doc, *args.docs)
+            Lo.save_doc(doc, Info.get_name(fnm) + "_APPENDED." + Info.get_ext(fnm))
+        finally:
+            Lo.close_doc(doc)
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
