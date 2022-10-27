857758148545437cc0c0ecc612e14f3d273edd6a
Barry-Thomas-Paul: Moss <vibrationoflife@protonmail.com>
2022-10-27 12:30:01 -0400

    upgrade to ooo-dev-tools 0.5.0

--- ex/auto/writer/odev_build_form/start.py
+++ ex/auto/writer/odev_build_form/start.py
@@ -1,8 +1,8 @@
 from pathlib import Path
-import uno
+
 from build_form import BuildForm
 from ooodev.utils.file_io import FileIO
-from ooodev.utils.lo import Lo
+
 
 def main() -> int:
     db_fnm = Path("resources", "odb", "liang.odb")
@@ -12,9 +12,10 @@ def main() -> int:
         p = FileIO.get_absolute_path(db_fnm)
     if not p.exists():
         raise FileNotFoundError("Unable to find path to liang.odb")
-    frm = BuildForm(p)
-    
+    _ = BuildForm(p)
+
     return 0
+
 
 if __name__ == "__main__":
     raise SystemExit(main())
