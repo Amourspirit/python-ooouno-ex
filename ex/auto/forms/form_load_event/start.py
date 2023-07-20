868bba041b8cff2db9769ff68604106f64228150
Barry-Thomas-Paul: Moss <vibrationoflife@protonmail.com>
2023-07-20 12:41:59 -0400

    update form load event

--- ex/auto/forms/form_load_event/start.py
+++ ex/auto/forms/form_load_event/start.py
@@ -5,14 +5,8 @@ from ooodev.utils.file_io import FileIO
 
 
 def main() -> int:
-    db_fnm = Path("resources", "odb", "Example_Sport.odb")
-    p = FileIO.get_absolute_path(db_fnm)
-    if not p.exists():
-        db_fnm = Path("../../../../resources", "odb", "Example_Sport.odb")
-        p = FileIO.get_absolute_path(db_fnm)
-    if not p.exists():
-        raise FileNotFoundError("Unable to find path to Example_Sport.odb")
-    _ = LoadListen(p)
+    db_fnm = Path(__file__).parent / "data" / "Example_Sport.odb"
+    _ = LoadListen(db_fnm)
 
     return 0
 
