5da9d2c2259257b760683f69449a30fd2630464a
Barry-Thomas-Paul: Moss <vibrationoflife@protonmail.com>
2023-07-23 07:26:38 -0400

    Update OooDev slide chart

--- ex/auto/chart2/slide_chart/start.py
+++ ex/auto/chart2/slide_chart/start.py
@@ -1,19 +1,12 @@
 from __future__ import annotations
 from pathlib import Path
 from slide_chart import SlideChart
-from ooodev.utils.file_io import FileIO
 
 
 # region main()
 def main() -> int:
 
-    fnm = Path("resources/ods/chartsData.ods")
-    p = FileIO.get_absolute_path(fnm)
-    if not p.exists():
-        fnm = Path("../../../../resources/ods/chartsData.ods")
-        p = FileIO.get_absolute_path(fnm)
-    if not p.exists():
-        raise FileNotFoundError("Unable to find path to chartsData.ods")
+    fnm = Path(__file__).parent / "data" / "chartsData.ods"
 
     sc = SlideChart(data_fnm=fnm)
     sc.main()
