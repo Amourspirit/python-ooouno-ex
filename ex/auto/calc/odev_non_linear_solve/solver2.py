c891f540e47da24097f3984aa3d7b0039564c01f
Barry-Thomas-Paul: Moss <vibrationoflife@protonmail.com>
2023-12-05 16:44:28 -0500

    update odev_non_linear_solve example

--- ex/auto/calc/odev_non_linear_solve/solver2.py
+++ ex/auto/calc/odev_non_linear_solve/solver2.py
@@ -4,7 +4,8 @@ import uno
 from com.sun.star.sheet import XSolver
 
 from ooodev.exceptions import ex
-from ooodev.office.calc import Calc
+from ooodev.calc import Calc
+from ooodev.calc import CalcDoc
 from ooodev.utils.lo import Lo
 from ooodev.utils.props import Props
 
@@ -12,40 +13,48 @@ from ooodev.utils.props import Props
 class Solver2:
     @staticmethod
     def main(verbose: bool = False) -> None:
-        with Lo.Loader(connector=Lo.ConnectPipe(), opt=Lo.Options(verbose=verbose)) as loader:
-            doc = Calc.create_doc(loader)
-            sheet = Calc.get_sheet(doc=doc)
+        with Lo.Loader(
+            connector=Lo.ConnectPipe(), opt=Lo.Options(verbose=verbose)
+        ) as loader:
+            doc = CalcDoc(Calc.create_doc(loader))
+            sheet = doc.get_sheet(0)
 
             # specify the variable cells
-            x_pos = Calc.get_cell_address(sheet=sheet, cell_name="B1")  # X
-            y_pos = Calc.get_cell_address(sheet=sheet, cell_name="B2")  # Y
+            x_pos = sheet.get_cell_address(cell_name="B1")  # X
+            y_pos = sheet.get_cell_address(cell_name="B2")  # Y
             vars = (x_pos, y_pos)
 
             # specify profit equation
-            Calc.set_val(value="=B1+B2", sheet=sheet, cell_name="B3")
-            objective = Calc.get_cell_address(sheet, "B3")
+            sheet.set_val(value="=B1+B2", cell_name="B3")
+            objective = sheet.get_cell_address(cell_name="B3")
 
             # set up equation formula without inequality (only one needed)
             # x^2 + y^2
-            Calc.set_val(value="=B1*B1 + B2*B2", sheet=sheet, cell_name="B4")
+            sheet.set_val(value="=B1*B1 + B2*B2", cell_name="B4")
 
             # create three constraints (using the 3 variables)
 
-            sc1 = Calc.make_constraint(num=1, op=">=", sheet=sheet, cell_name="B4")
+            sc1 = sheet.make_constraint(num=1, op=">=", cell_name="B4")
             #   x^2 + y^2 >= 1
-            sc2 = Calc.make_constraint(num=2, op="<=", sheet=sheet, cell_name="B4")
+            sc2 = sheet.make_constraint(num=2, op="<=", cell_name="B4")
             #   x^2 + y^2 <= 2
 
             constraints = (sc1, sc2)
 
             # initialize the nonlinear solver (SCO)
             try:
-                solver = Lo.create_instance_mcf(XSolver, "com.sun.star.comp.Calc.NLPSolver.SCOSolverImpl", raise_err=True)
-            except ex.MissingInterfaceError:
-                print('Solver not available on this system: "com.sun.star.comp.Calc.NLPSolver.SCOSolverImpl"')
-                Lo.close_doc(doc)
+                solver = Lo.create_instance_mcf(
+                    XSolver,
+                    "com.sun.star.comp.Calc.NLPSolver.SCOSolverImpl",
+                    raise_err=True,
+                )
+            except ex.CreateInstanceMcfError:
+                print(
+                    'Solver not available on this system: "com.sun.star.comp.Calc.NLPSolver.SCOSolverImpl"'
+                )
+                doc.close_doc()
                 return
-            solver.Document = doc
+            solver.Document = doc.component
             solver.Objective = objective
             solver.Variables = vars
             solver.Constraints = constraints
@@ -59,4 +68,4 @@ class Solver2:
             # execute the solver
             solver.solve()
             Calc.solver_report(solver)
-            Lo.close_doc(doc)
+            doc.close_doc()
