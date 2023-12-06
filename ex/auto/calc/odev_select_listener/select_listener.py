759ad9d8e445e55d02bf3a3f6d9a371ca7b25876
Barry-Thomas-Paul: Moss <vibrationoflife@protonmail.com>
2023-12-05 19:33:18 -0500

    update odev_select_listener

--- ex/auto/calc/odev_select_listener/select_listener.py
+++ ex/auto/calc/odev_select_listener/select_listener.py
@@ -9,7 +9,8 @@ from ooo.dyn.table.cell_address import CellAddress
 from ooodev.adapter.awt.top_window_events import TopWindowEvents
 from ooodev.adapter.view.selection_change_events import SelectionChangeEvents
 from ooodev.events.args.event_args import EventArgs
-from ooodev.office.calc import Calc
+from ooodev.calc import Calc
+from ooodev.calc import CalcDoc
 from ooodev.utils.gui import GUI
 from ooodev.utils.lo import Lo
 
@@ -23,28 +24,30 @@ class SelectionListener:
         super().__init__()
         self.closed = False
         loader = Lo.load_office(Lo.ConnectSocket())
-        self._doc = Calc.create_doc(loader)
+        self._doc = CalcDoc(Calc.create_doc(loader))
 
-        GUI.set_visible(visible=True, doc=self._doc)
-        self.sheet = Calc.get_sheet(doc=self._doc, index=0)
+        self._doc.set_visible()
+        self._sheet = self._doc.get_sheet(0)
 
-        self.curr_addr = Calc.get_selected_cell_addr(self._doc)
-        self.curr_val = self._get_cell_float(self.curr_addr)  # may be None
+        self._curr_addr = self._doc.get_selected_cell_addr()
+        self._curr_val = self._get_cell_float(self._curr_addr)  # may be None
 
         self._attach_listener()
 
         # insert some data
-        Calc.set_col(sheet=self.sheet, cell_name="A1", values=("Smith", 42, 58.9, -66.5, 43.4, 44.5, 45.3))
+        self._sheet.set_col(
+            values=("Smith", 42, 58.9, -66.5, 43.4, 44.5, 45.3),
+            cell_name="A1",
+        )
 
     def _get_cell_float(self, addr: CellAddress) -> float | None:
         # get_val returns floats as float instance
-        obj = Calc.get_val(sheet=self.sheet, addr=addr)
+        obj = self._sheet.get_val(addr=addr)
         if isinstance(obj, float):
             return obj
         return None
 
     def _attach_listener(self) -> None:
-
         # Event handlers are defined as methods on the class.
         # However class methods are not callable by the event system.
         # The solution is to assign the method to class fields and use them to add the event callbacks.
@@ -57,21 +60,24 @@ class SelectionListener:
         self._twe.add_event_window_closing(self._fn_on_window_closing)
 
         # pass doc to constructor, this will allow listener events to be automatically attached to document.
-        self._sel_events = SelectionChangeEvents(doc=self._doc)
+        self._sel_events = SelectionChangeEvents(doc=self._doc.component)
         self._sel_events.add_event_selection_changed(self._on_selection_changed)
         self._sel_events.add_event_selection_change_events_disposing(self._on_disposing)
 
-
-    def on_window_closing(self, source: Any, event_args: EventArgs, *args, **kwargs) -> None:
+    def on_window_closing(
+        self, source: Any, event_args: EventArgs, *args, **kwargs
+    ) -> None:
         print("Closing")
         try:
-            Lo.close_doc(self._doc)
+            self._doc.close_doc()
             Lo.close_office()
             self.closed = True
         except Exception as e:
             print(f"  {e}")
 
-    def on_selection_changed(self, source: Any, event_args: EventArgs, *args, **kwargs) -> None:
+    def on_selection_changed(
+        self, source: Any, event_args: EventArgs, *args, **kwargs
+    ) -> None:
         # is fired four times for every click, and twice for shift arrow keys (?)
         # Once for arrow keys.
         # - Report when the selection changes by printing the name of the
@@ -84,28 +90,30 @@ class SelectionListener:
             print("No ctrl for event source")
             return
 
-        addr = Calc.get_selected_cell_addr(self._doc)
+        addr = self._doc.get_selected_cell_addr()
         if addr is None:
             return
         try:
             # better to wrap in try block.
             # otherwise errors crashes office
-            if not Calc.is_equal_addresses(addr, self.curr_addr):
-                flt = self._get_cell_float(self.curr_addr)
+            if not Calc.is_equal_addresses(addr, self._curr_addr):
+                flt = self._get_cell_float(self._curr_addr)
                 if flt is not None:
-                    if self.curr_val is None:  # so previously stored value was null
-                        print(f"{Calc.get_cell_str(self.curr_addr)} new value: {flt:.2f}")
+                    if self._curr_val is None:  # so previously stored value was null
+                        print(
+                            f"{Calc.get_cell_str(self._curr_addr)} new value: {flt:.2f}"
+                        )
                     else:
-                        if self.curr_val != flt:
+                        if self._curr_val != flt:
                             print(
-                                f"{Calc.get_cell_str(self.curr_addr)} has changed from {self.curr_val:.2f} to {flt:.2f}"
+                                f"{Calc.get_cell_str(self._curr_addr)} has changed from {self._curr_val:.2f} to {flt:.2f}"
                             )
 
             # update current address and value
-            self.curr_addr = addr
-            self.curr_val = self._get_cell_float(addr)
-            if self.curr_val is not None:
-                print(f"{Calc.get_cell_str(self.curr_addr)} value: {self.curr_val}")
+            self._curr_addr = addr
+            self._curr_val = self._get_cell_float(addr)
+            if self._curr_val is not None:
+                print(f"{Calc.get_cell_str(self._curr_addr)} value: {self._curr_val}")
         except Exception as e:
             print(e)
 
