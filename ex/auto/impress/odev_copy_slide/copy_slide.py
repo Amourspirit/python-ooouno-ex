60246b70b41ea0124c470f743fea4208869a562a
Barry-Thomas-Paul: Moss <vibrationoflife@protonmail.com>
2023-12-12 16:41:55 -0500

    update odev_copy_slide example

--- ex/auto/impress/odev_copy_slide/copy_slide.py
+++ ex/auto/impress/odev_copy_slide/copy_slide.py
@@ -1,22 +1,19 @@
 from __future__ import annotations
-from typing import TYPE_CHECKING
-
 
 import uno
-from ooodev.dialog.msgbox import MsgBox, MessageBoxType, MessageBoxButtonsEnum, MessageBoxResultsEnum
+from ooodev.dialog.msgbox import (
+    MsgBox,
+    MessageBoxType,
+    MessageBoxButtonsEnum,
+    MessageBoxResultsEnum,
+)
 from ooodev.utils.dispatch.draw_view_dispatch import DrawViewDispatch
 from ooodev.utils.dispatch.draw_drawing_dispatch import DrawDrawingDispatch
 from ooodev.utils.dispatch.global_edit_dispatch import GlobalEditDispatch
-from ooodev.office.draw import Draw
+from ooodev.draw import ImpressDoc
 from ooodev.utils.file_io import FileIO
-from ooodev.utils.gui import GUI
 from ooodev.utils.lo import Lo
 from ooodev.utils.type_var import PathOrStr
-
-if TYPE_CHECKING:
-    # the following is only needed for typings.
-    # from __future__ import annotations takes care of the rest
-    from com.sun.star.lang import XComponent
 
 
 class CopySlide:
@@ -36,13 +33,13 @@ class CopySlide:
         loader = Lo.load_office(Lo.ConnectPipe())
 
         try:
-            doc = Lo.open_doc(fnm=self._fnm, loader=loader)
-            num_slides = Draw.get_slides_count(doc)
+            doc = ImpressDoc(Lo.open_doc(fnm=self._fnm, loader=loader))
+            num_slides = doc.get_slides_count()
             if self._from_idx >= num_slides or self._to_idx >= num_slides:
                 Lo.close_office()
                 raise IndexError("One or both indices are out of range")
 
-            GUI.set_visible(visible=True, doc=doc)
+            doc.set_visible()
 
             self._copy_to(doc=doc)
 
@@ -59,7 +56,7 @@ class CopySlide:
                 buttons=MessageBoxButtonsEnum.BUTTONS_YES_NO,
             )
             if msg_result == MessageBoxResultsEnum.YES:
-                Lo.close_doc(doc=doc, deliver_ownership=True)
+                doc.close_doc()
                 Lo.close_office()
             else:
                 print("Keeping document open")
@@ -69,11 +66,9 @@ class CopySlide:
             Lo.close_office()
             raise
 
-    def _copy_to(self, doc: XComponent) -> None:
+    def _copy_to(self, doc: ImpressDoc) -> None:
         # Copy fromIdx slide to the clipboard in slide-sorter mode,
         # then paste it to after the toIdx slide.
-
-        ctrl = GUI.get_current_controller(doc)
 
         # Switch to slide sorter view so that slides can be pasted
         Lo.delay(1000)
@@ -82,15 +77,14 @@ class CopySlide:
         # give Office a few seconds of time to do it
         Lo.delay(3000)
 
-        from_slide = Draw.get_slide(doc, self._from_idx)
-        to_slide = Draw.get_slide(doc, self._to_idx)
-
-        Draw.goto_page(ctrl, from_slide)
+        from_slide = doc.get_slide(idx=self._from_idx)
+        to_slide = doc.get_slide(idx=self._to_idx)
+        doc.goto_page(from_slide.component)
         Lo.dispatch_cmd(cmd=GlobalEditDispatch.COPY)
         Lo.delay(500)
         print(f"Copied {self._from_idx}")
 
-        Draw.goto_page(ctrl, to_slide)
+        doc.goto_page(to_slide.component)
         Lo.delay(500)
         Lo.dispatch_cmd(GlobalEditDispatch.PASTE)
         Lo.delay(500)
