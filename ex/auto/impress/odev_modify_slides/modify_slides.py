cb49f6a8aa61cfc9de31a0ab8b9bcb4801051034
Barry-Thomas-Paul: Moss <vibrationoflife@protonmail.com>
2023-12-13 12:05:01 -0500

    update odev_modify_slides example

--- ex/auto/impress/odev_modify_slides/modify_slides.py
+++ ex/auto/impress/odev_modify_slides/modify_slides.py
@@ -1,10 +1,14 @@
 from __future__ import annotations
 
 import uno
-from ooodev.dialog.msgbox import MsgBox, MessageBoxType, MessageBoxButtonsEnum, MessageBoxResultsEnum
-from ooodev.office.draw import Draw
+from ooodev.dialog.msgbox import (
+    MsgBox,
+    MessageBoxType,
+    MessageBoxButtonsEnum,
+    MessageBoxResultsEnum,
+)
+from ooodev.draw import ImpressDoc, ImpressPage
 from ooodev.utils.file_io import FileIO
-from ooodev.utils.gui import GUI
 from ooodev.utils.info import Info
 from ooodev.utils.lo import Lo
 from ooodev.utils.type_var import PathOrStr
@@ -21,28 +25,34 @@ class ModifySlides:
         loader = Lo.load_office(Lo.ConnectPipe())
 
         try:
-            doc = Lo.open_doc(self._fnm, loader)
+            doc_component = Lo.open_doc(self._fnm, loader)
 
             # slideshow start() crashes if the doc is not visible
-            GUI.set_visible(visible=True, doc=doc)
 
-            if not Info.is_doc_type(obj=doc, doc_type=Lo.Service.IMPRESS):
+            if not Info.is_doc_type(obj=doc_component, doc_type=Lo.Service.IMPRESS):
                 print("-- Not a slides presentation")
                 Lo.close_office()
                 return
 
-            slides = Draw.get_slides(doc)
-            num_slides = slides.getCount()
+            doc = ImpressDoc(doc_component)
+            doc.set_visible()
+
+            slides = doc.get_slides()
+            num_slides = slides.get_count()
             print(f"No. of slides: {num_slides}")
 
             # add a title-only slide with a graphic at the end
-            last_page = slides.insertNewByIndex(num_slides)
-            Draw.title_only_slide(slide=last_page, header="Any Questions?")
-            Draw.draw_image(slide=last_page, fnm=self._im_fnm)
+            last_page = ImpressPage(
+                owner=doc, component=slides.insert_new_by_index(num_slides)
+            )
+            last_page.title_only_slide(header="Any Questions?")
+            last_page.draw_image(fnm=self._im_fnm)
 
             # add a title/subtitle slide at the start
-            first_page = slides.insertNewByIndex(0)
-            Draw.title_slide(slide=first_page, title="Interesting Slides", sub_title="Brought to you by OooDev")
+            first_page = ImpressPage(owner=doc, component=slides.insert_new_by_index(0))
+            first_page.title_slide(
+                title="Interesting Slides", sub_title="Brought to you by OooDev"
+            )
 
             Lo.delay(2000)
             msg_result = MsgBox.msgbox(
@@ -52,7 +62,7 @@ class ModifySlides:
                 buttons=MessageBoxButtonsEnum.BUTTONS_YES_NO,
             )
             if msg_result == MessageBoxResultsEnum.YES:
-                Lo.close_doc(doc=doc, deliver_ownership=True)
+                doc.close_doc()
                 Lo.close_office()
             else:
                 print("Keeping document open")
