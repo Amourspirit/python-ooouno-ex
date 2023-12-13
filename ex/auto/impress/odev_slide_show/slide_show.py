0ccf4362a468155eaf2fe1e871a9e63007bd8ab4
Barry-Thomas-Paul: Moss <vibrationoflife@protonmail.com>
2023-12-13 12:35:59 -0500

    updated odev_slide_show example

--- ex/auto/impress/odev_slide_show/slide_show.py
+++ ex/auto/impress/odev_slide_show/slide_show.py
@@ -1,10 +1,21 @@
 from __future__ import annotations
 
 import uno
-from ooodev.dialog.msgbox import MsgBox, MessageBoxType, MessageBoxButtonsEnum, MessageBoxResultsEnum
-from ooodev.office.draw import Draw, FadeEffect, AnimationSpeed, DrawingGradientKind, DrawingSlideShowKind
+from ooodev.dialog.msgbox import (
+    MsgBox,
+    MessageBoxType,
+    MessageBoxButtonsEnum,
+    MessageBoxResultsEnum,
+)
+from ooodev.draw import (
+    Draw,
+    ImpressDoc,
+    FadeEffect,
+    AnimationSpeed,
+    DrawingGradientKind,
+    DrawingSlideShowKind,
+)
 from ooodev.utils.dispatch.draw_view_dispatch import DrawViewDispatch
-from ooodev.utils.gui import GUI
 from ooodev.utils.lo import Lo
 from ooodev.utils.props import Props
 
@@ -18,74 +29,94 @@ class SlideShow:
 
         # create Impress page or Draw slide
         try:
-            doc = Draw.create_impress_doc(loader)
+            doc = ImpressDoc(Draw.create_impress_doc(loader))
 
-            while Draw.get_slides_count(doc) < 3:
-                _ = Draw.add_slide(doc)
+            while doc.get_slides_count() < 3:
+                _ = doc.add_slide()
 
             # ---- The first page
-            slide = Draw.get_slide(doc=doc, idx=0)
-            Draw.set_transition(
-                slide=slide,
+            slide = doc.get_slide(idx=0)
+            slide.set_transition(
                 fade_effect=FadeEffect.FADE_FROM_RIGHT,
                 speed=AnimationSpeed.FAST,
                 change=DrawingSlideShowKind.AUTO_CHANGE,
                 duration=1,
             )
             # draw a square at the top left of the page; and text
-            sq1 = Draw.draw_rectangle(slide=slide, x=10, y=10, width=50, height=50)
-            Props.set(sq1, Effect=AnimationEffect.WAVYLINE_FROM_BOTTOM)
+            sq1 = slide.draw_rectangle(x=10, y=10, width=50, height=50)
+            sq1.set_property(Effect=AnimationEffect.WAVYLINE_FROM_BOTTOM)
             # square appears in 'wave' (pixels by pixels)
-            _ = Draw.draw_text(slide=slide, msg="Page 1", x=70, y=20, width=60, height=30, font_size=24)
+            _ = slide.draw_text(
+                msg="Page 1", x=70, y=20, width=60, height=30, font_size=24
+            )
 
             # ---- The second page
-            slide = Draw.get_slide(doc=doc, idx=1)
-            Draw.set_transition(
-                slide=slide,
+            slide = doc.get_slide(idx=1)
+            slide.set_transition(
                 fade_effect=FadeEffect.FADE_FROM_RIGHT,
                 speed=AnimationSpeed.FAST,
                 change=DrawingSlideShowKind.AUTO_CHANGE,
                 duration=1,
             )
             # draw a circle at the bottom right of second page; and text
-            circle1 = Draw.draw_ellipse(slide=slide, x=212, y=150, width=50, height=50)
+            circle1 = slide.draw_ellipse(x=212, y=150, width=50, height=50)
             # hide circle after drawing
-            Props.set(circle1, Effect=AnimationEffect.HIDE)
+            circle1.set_property(Effect=AnimationEffect.HIDE)
 
-            _ = Draw.draw_text(slide=slide, msg="Page 2", x=170, y=170, width=60, height=30, font_size=24)
+            _ = slide.draw_text(
+                msg="Page 2",
+                x=170,
+                y=170,
+                width=60,
+                height=30,
+                font_size=24,
+            )
 
             name_slide = "page two"
-            Draw.set_name(slide=slide, name=name_slide)
+            slide.set_name(name_slide)
 
             # ---- The third page
-            slide = Draw.get_slide(doc=doc, idx=2)
-            Draw.set_transition(
-                slide=slide,
+            slide = doc.get_slide(idx=2)
+            slide.set_transition(
                 fade_effect=FadeEffect.ROLL_FROM_LEFT,
                 speed=AnimationSpeed.MEDIUM,
                 change=DrawingSlideShowKind.AUTO_CHANGE,
                 duration=2,
             )
-            _ = Draw.draw_text(slide=slide, msg="Page 3", x=120, y=75, width=60, height=30, font_size=24)
+            _ = slide.draw_text(
+                msg="Page 3",
+                x=120,
+                y=75,
+                width=60,
+                height=30,
+                font_size=24,
+            )
             # draw a circle containing text
-            circle2 = Draw.draw_ellipse(slide=slide, x=10, y=8, width=50, height=50)
-            Draw.add_text(shape=circle2, msg="Click to go\nto Page1")
-            Draw.set_gradient_color(shape=circle2, name=DrawingGradientKind.MAHOGANY)
+            circle2 = slide.draw_ellipse(x=10, y=8, width=50, height=50)
+            circle2.add_text(msg="Click to go\nto Page1")
+            circle2.set_gradient_color(name=DrawingGradientKind.MAHOGANY)
 
             # clicking makes the presentation jump to page one
-            Props.set(circle2, Effect=AnimationEffect.FADE_FROM_BOTTOM, OnClick=ClickAction.FIRSTPAGE)
+            circle2.set_property(
+                Effect=AnimationEffect.FADE_FROM_BOTTOM,
+                OnClick=ClickAction.FIRSTPAGE,
+            )
 
             # draw a square with text
-            sq2 = Draw.draw_rectangle(slide=slide, x=220, y=8, width=50, height=50)
-            Draw.add_text(shape=sq2, msg="Click to go\nto Page 2")
-            Draw.set_gradient_color(shape=sq2, name=DrawingGradientKind.MAHOGANY)
+            sq2 = slide.draw_rectangle(x=220, y=8, width=50, height=50)
+            sq2.add_text(msg="Click to go\nto Page 2")
+            sq2.set_gradient_color(name=DrawingGradientKind.MAHOGANY)
 
             # clicking makes the presentation jump to page two by using a bookmark
-            Props.set(sq2, Effect=AnimationEffect.FADE_FROM_BOTTOM, OnClick=ClickAction.BOOKMARK, Bookmark=name_slide)
+            sq2.set_property(
+                Effect=AnimationEffect.FADE_FROM_BOTTOM,
+                OnClick=ClickAction.BOOKMARK,
+                Bookmark=name_slide,
+            )
 
             # slideshow start() crashes if the doc is not visible
-            GUI.set_visible(visible=True, doc=doc)
-            show = Draw.get_show(doc)
+            doc.set_visible()
+            show = doc.get_show()
 
             # a full-screen slide show
             Lo.dispatch_cmd(DrawViewDispatch.PRESENTATION)
@@ -94,7 +125,7 @@ class SlideShow:
             # show.start()
 
             Props.show_obj_props("Slide show", show)
-            sc = Draw.get_show_controller(show)
+            sc = doc.get_show_controller()
 
             # Draw.wait_last(sc, 2000)
             # Lo.dispatch_cmd(DrawViewDispatch.PRESENTATION_END)
@@ -107,7 +138,7 @@ class SlideShow:
                 buttons=MessageBoxButtonsEnum.BUTTONS_YES_NO,
             )
             if msg_result == MessageBoxResultsEnum.YES:
-                Lo.close_doc(doc=doc, deliver_ownership=True)
+                doc.close_doc()
                 Lo.close_office()
             else:
                 print("Keeping document open")
