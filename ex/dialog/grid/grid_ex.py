c4d543ce1bbc028cf0bfae8af76e4617c1e65268
Barry-Thomas-Paul: Moss <4193389+Amourspirit@users.noreply.github.com>
2024-03-27 17:15:08 -0400

    Develop (#324)

--- ex/dialog/grid/grid_ex.py
+++ ex/dialog/grid/grid_ex.py
@@ -205,3 +205,21 @@ class GridEx:
         return result
 
     # endregion Show Dialog
+
+
+# region Macro method
+
+# show_grid is here and in script.py
+# The reason for this is when this script is embedded into a document via make build it is a stand alone script.
+# As a standalone script there needs to be a macro to call.
+# When embedded using make build_ooodev it is part of a multi-file output and the macro is not needed and will be found in script.py
+
+
+def show_grid(*args) -> None:
+    doc = CalcDoc.from_current_doc()
+    grid_ex = GridEx(doc=doc)
+    grid_ex.show()
+
+
+g_exportedScripts = (show_grid,)
+# endregion Macro method
